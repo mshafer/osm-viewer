65e3f25d08b35a26271ce259a8ed8b02288a0b1e
Michael Shafer <mshafer3@gmail.com>
2015-04-17 16:56:42 +1200

    Angular app is up and running with leaflet directive

--- osmviewer.py
+++ osmviewer.py
@@ -1,11 +1,26 @@
-from flask import Flask
+import json
+from flask import Flask, render_template, jsonify, Response
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def map_view():
+    return render_template('map-view.html')
+
+
+@app.route('/api/points')
+def points():
+    points = [
+        {
+            'osmId': '2433178371.0',
+            'name':  'Caroline Chisholm Drive near Junction Road',
+            'type': 'bus',
+            'latitude': 150.9719909,
+            'longitude': -33.7745365
+        }
+    ]
+    return Response(json.dumps(points),  mimetype='application/json')
 
 
 if __name__ == '__main__':
---
+++ static/js/app.js
@@ -0,0 +1,131 @@
+/**
+ * Created by mshafer on 17/04/15.
+ */
+
+(function($, angular) {
+    var API_BASE_URL = '/api';
+    var API_POINTS_ENDPOINT = '/points';
+    var MAPBOX_ID = 'mshafer.lofi3idm';
+
+    var app = angular.module('osmviewer', ['leaflet-directive']);
+
+    app.controller('MapController', ['$scope', 'leafletData', 'pointsService', function($scope, leafletData, pointsService) {
+        angular.extend($scope, {
+            defaults: {
+                tileLayer: 'http://{s}.tiles.mapbox.com/v3/'+ MAPBOX_ID + '/{z}/{x}/{y}.png',
+                maxZoom: 18
+            },
+            sydney: {
+                lat: -33.8658763,
+                lng: 151.21242949999998,
+                zoom: 15
+            },
+            markers: {},
+            events: {
+                map: {
+                    enable: ['load'],
+                    logic: 'emit'
+                }
+            }
+        });
+
+        $scope.$on('leafletDirectiveMap.load', function(event) {
+            fetchPointsWithinBounds();
+        });
+
+        function fetchPointsWithinBounds() {
+            console.log(leafletData.getMap());
+            leafletData.getMap().then(function(map) {
+                var bounds = map.getBounds();
+                var fetchPointsSuccess = function(points) {
+                    points.forEach(function(point) {
+                        console.log(point);
+                    });
+                };
+
+                pointsService.getPoints(bounds).success(fetchPointsSuccess);
+            });
+        }
+    }]);
+
+    app.factory('pointsService', ['$http', function($http) {
+        var service = {
+            /**
+             * Make an API call to fetch all the points within the specified bounds
+             *
+             * @param bounds A LatLngBounds instance
+             */
+            getPoints: function(bounds) {
+                var requestParams = {
+                    north: bounds.getNorth(),
+                    east: bounds.getEast(),
+                    south: bounds.getSouth(),
+                    west: bounds.getWest()
+                };
+
+                return $http({
+                    url: API_BASE_URL + API_POINTS_ENDPOINT,
+                    method: 'GET',
+                    params: requestParams
+                });
+            }
+        };
+
+        return service;
+    }]);
+
+//    // This hashtable is used to keep track of which points we've already added to the map.
+//    // Uses the point's osmId as the key
+//    var existingPointsByOsmId = {};
+//
+//    var map = L.map('map-view').setView(STARTING_COORDINATES, INITIAL_ZOOM);
+//    map.on('load', fetchPointsInBounds(map.getBounds()));
+//
+//    L.tileLayer(, {
+//        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//        maxZoom: 18
+//    }).addTo(map);
+//
+//    /**
+//     * Make an API call to get all the points (bus and train stops) within the given bounds.
+//     *
+//     * @param bounds A LatLngBounds instance
+//     */
+//    function fetchPointsInBounds(bounds) {
+//        var requestData = {
+//            north: bounds.getNorth(),
+//            east: bounds.getEast(),
+//            south: bounds.getSouth(),
+//            west: bounds.getWest()
+//        };
+//
+//        $.ajax({
+//            method: 'GET',
+//            url: API_BASE_URL + API_POINTS_ENDPOINT,
+//            data: requestData
+//        }).done(createMarkersFromPoints);
+//    }
+//
+//    /**
+//     * Given a list of points (bus and train stops), add leaflet markers to the map.
+//     *
+//     * @param points
+//     */
+//    function createMarkersFromPoints(points) {
+//        busStopMarkers.clearLayers();
+//        trainStopMarkers.clearLayers();
+//
+//        points.forEach(function(point) {
+//            L.marker([point.latitude, point.longitude], {
+//                title: point['name']
+//            }).addTo(map);
+//
+//            if (point['type'] === "train") {
+//                trainStopMarkers.addLayer(marker);
+//            } else {
+//                busStopMarkers.addLayer(marker);
+//            }
+//        });
+//    }
+
+})(jQuery, angular);
